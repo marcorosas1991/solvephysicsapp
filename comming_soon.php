a8d0513f4f3477539842acc9dc3b0dfbe930f84c
Marco Rosas <marco.rosas@outlook.com>
2015-08-27 19:28:54 -0500

    update

--- comming_soon.php
+++ comming_soon.php
@@ -35,4 +35,21 @@
   </form>
 </div>
 
+<div class="">
+  <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="5DZX3QBV9ZLMS">
+<table>
+<tr><td><input type="hidden" name="on0" value="Subscription options">Subscription options</td></tr><tr><td><select name="os0">
+	<option value="12 month subscription">12 month subscription : $8.99 USD - yearly</option>
+	<option value="1 month subscription">1 month subscription : $0.99 USD - monthly</option>
+</select> </td></tr>
+</table>
+<input type="hidden" name="currency_code" value="USD">
+<input type="image" src="https://www.paypalobjects.com/en_US/MX/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+</div>
+
 <?php include "view/footer.php" ?>
