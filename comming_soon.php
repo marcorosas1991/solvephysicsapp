987a415646fb426cb10766544746249d64ad4376
Marco Rosas <marco.rosas@outlook.com>
2015-09-15 16:05:34 -0500

    download_from_app_store

--- comming_soon.php
+++ comming_soon.php
@@ -11,6 +11,10 @@
   <h1>Solve Physics</h1>
 </div>
 
+<div class="download_app">
+  <a href="https://geo.itunes.apple.com/mx/app/solve-physics/id1033396034?mt=8" style="display:inline-block;overflow:hidden;background:url(http://linkmaker.itunes.apple.com/images/badges/en-us/badge_appstore-lrg.svg) no-repeat;width:165px;height:40px;"></a>
+</div>
+
 <div id="main_text">
   <p>
     <?php echo $description_1[$language] ?>
