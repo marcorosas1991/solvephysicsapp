73f1d2e0655181190653c6d0b4cbd46b5b65d8b8
Marco Rosas <marco.rosas@outlook.com>
2015-09-24 22:55:35 -0500

    banner

--- view/header.php
+++ view/header.php
@@ -4,5 +4,6 @@
     <meta charset="utf-8">
     <title>Solve Physics</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta name="Solve Physics" content="app-id=1033396034">
   </head>
   <body>
